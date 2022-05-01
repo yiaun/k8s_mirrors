cee7224537f7439eaa8d0190077e3ce40df9d7e7
Vex <36614122+Asvex@users.noreply.github.com>
2022-05-01 12:52:38 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-version=v3.22.1
+calico_version=v3.22.1
+coredns_version=1.8.7
 hub=registry.cn-hangzhou.aliyuncs.com
 
-for i in caliao/cni
-docker pull $i:$version
-docker tag $i:$vserion $hub/asvex/$i:$version
-docker push $hub/asvex/$i:$version
+for i in `cat calico`
+do
+  docker pull calico/$i:$calico_version
+  docker tag  calico/$i:$vserion $hub/asvex/$i:$calico_version
+  docker push $hub/asvex/$i:$calico_version
+done
+
+docker pull coredns/coredns:1.8.7
+docker tag coredns/coredns:1.8.7 $hub/asvex/$i:coredns_version
+docker push $hub/asvex/$i:$coredns_version
